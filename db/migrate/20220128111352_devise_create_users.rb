b9ad081e684e039e8239303bf4efa23282f1dbfe
tsukasakaiko <kaikobt825@gmail.com>
2022-01-28 22:09:31 +0900

    birth_dayをdateに変更

--- db/migrate/20220128111352_devise_create_users.rb
+++ db/migrate/20220128111352_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true 
       t.string :encrypted_password, null: false, default: ""
+
+      t.string :nickname, null: false
+      t.string :family_name, null: false
+      t.string :farst_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :farst_name_kana, null: false
+      t.date :birth_day, null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
