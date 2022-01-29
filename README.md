1f3fdcea65b47e9072aae668f079202dad77d837
tsukasakaiko <kaikobt825@gmail.com>
2022-01-29 11:38:45 +0900

    新規登録/本人情報確認実装

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ usersテーブル
 | Column             | Type                | Options                   |
 |--------------------|---------------------|---------------------------|
 | nickname           | string              | null: false,              |
-| email              | string              | null: false,unique: true  |
+| email              | string              | null: false, unique: true |
 | encrypted_password | string              | null: false               |
 | family_name        | string              | null: false               |
 | farst_name         | string              | null: false               |
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :family_name, presence: true 
+  validates :farst_name, presence: true 
+
+  validates :family_name_kana, presence: true  
+  validates :farst_name_kana, presence: true 
+  end
+
+  validates :birth_day, presence: true
+
+  
+
 end
