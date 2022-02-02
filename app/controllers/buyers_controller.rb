10e5cd6a608354e75533bfb22fdc8196cd79116f
tsukasakaiko <kaikobt825@gmail.com>
2022-02-02 15:31:43 +0900

    buyerとアドレスモデル作成

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -3,14 +3,11 @@ class BuyersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    if @item.user.id != current_user.id && @item.buyers.blank?
-      @sendding_address = SenddingAddress.new
-    else
-      redirect_to root_path
-    end
+
   end
 
   def new
   end
 
 end
+
--- app/models/sending_address.rb
+++ app/models/sending_address.rb
@@ -1,2 +1,7 @@
 class SendingAddress < ApplicationRecord
+  belongs_to :buyer
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 end
